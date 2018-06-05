1b1b100232f7bd33f92a2929ea41bbb3e31f516f
Michał Siejak <michal@siejak.pl>
2018-06-05 21:28:31 +0200

    UE4: Added prebuilt ENET library for Win64 platform

--- plugins/UnrealPlugin/Plugins/RemotePlay/Source/RemotePlay/RemotePlay.Build.cs
+++ plugins/UnrealPlugin/Plugins/RemotePlay/Source/RemotePlay/RemotePlay.Build.cs
@@ -54,20 +54,12 @@ public class RemotePlay : ModuleRules
 			);
 
         Link_libavstream(Target);
+        Link_libenet(Target);
 	}
 
     private void Link_libavstream(ReadOnlyTargetRules Target)
     {
-        string LibraryPath = null;
-        switch (Target.Platform)
-        {
-            case UnrealTargetPlatform.Win64:
-                LibraryPath = Path.Combine(LibrariesDirectory, "libavstream/Win64");
-                break;
-            case UnrealTargetPlatform.Win32:
-                LibraryPath = Path.Combine(LibrariesDirectory, "libavstream/Win32");
-                break;
-        }
+        string LibraryPath = Path.Combine(LibrariesDirectory, "libavstream", GetPlatformName(Target));
 
         PrivateIncludePaths.Add(Path.Combine(LibrariesDirectory, "libavstream/Include"));
         PublicLibraryPaths.Add(LibraryPath);
@@ -75,6 +67,26 @@ public class RemotePlay : ModuleRules
         PublicAdditionalLibraries.Add("libavstream.lib");
         PublicDelayLoadDLLs.Add("libavstream.dll");
         RuntimeDependencies.Add(Path.Combine(LibraryPath, "libavstream.dll"));
+    }
+
+    private void Link_libenet(ReadOnlyTargetRules Target)
+    {
+        PrivateIncludePaths.Add(Path.Combine(LibrariesDirectory, "enet/Include"));
+        PublicLibraryPaths.Add(Path.Combine(LibrariesDirectory, "enet", GetPlatformName(Target)));
+        PublicAdditionalLibraries.Add("enet.lib");
+    }
+
+    private string GetPlatformName(ReadOnlyTargetRules Target)
+    {
+        switch(Target.Platform)
+        {
+            case UnrealTargetPlatform.Win64:
+                return "Win64";
+            case UnrealTargetPlatform.Win32:
+                return "Win32";
+            default:
+                return "Unknown";
+        }
     }
 
     private string LibrariesDirectory
