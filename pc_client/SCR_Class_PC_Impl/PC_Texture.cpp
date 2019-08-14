af40441f7dd4620a6dd7385dad74357c042a69d3
Roderick Kennedy <roderick.kennedy@gmail.com>
2019-08-14 17:30:12 +0100

    Implement Create()

--- pc_client/SCR_Class_PC_Impl/PC_Texture.cpp
+++ pc_client/SCR_Class_PC_Impl/PC_Texture.cpp
@@ -33,6 +33,14 @@ void PC_Texture::Unbind() const
 void PC_Texture::UseSampler(const Sampler* sampler)
 {
 }
+
+void PC_Texture::Create(TextureCreateInfo * pTextureCreateInfo)
+{
+	m_CI = *pTextureCreateInfo;
+	//m_CI.size = pTextureCreateInfo->width * pTextureCreateInfo->height * pTextureCreateInfo->depth *pTextureCreateInfo->bitsPerPixel;
+	//m_Data = data;
+}
+
 void PC_Texture::GenerateMips()
 {
 }
