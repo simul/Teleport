262ec0c3f6378b29a3f012e70a7d638c208565cd
Roderick Kennedy <rvkennedy@users.noreply.github.com>
2019-08-14 17:39:20 +0100

    Update PC_Texture.cpp
    Remove extra fn.

--- pc_client/SCR_Class_PC_Impl/PC_Texture.cpp
+++ pc_client/SCR_Class_PC_Impl/PC_Texture.cpp
@@ -4,21 +4,6 @@
 using namespace pc_client;
 using namespace scr;
 
-void PC_Texture::Create(Slot slot, Type type, Format format, SampleCountBit sampleCount, uint32_t width, uint32_t height, uint32_t depth, uint32_t bytesPerPixel, const uint8_t* data)
-{
-    m_Width = width;
-    m_Height = height;
-    m_Depth = depth;
-    m_BytesPerPixel = bytesPerPixel;
-
-    m_Slot = slot;
-    m_Type = type;
-    m_Format = format;
-    m_SampleCount = sampleCount;
-
-    m_Size = m_Width * m_Height * m_Depth * bytesPerPixel;
-    m_Data = data;
-}
 void PC_Texture::Destroy()
 {
 }
