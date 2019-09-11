5d08be5b733728d4391c02e8931a491d1b2507c4
Aidan Clear <aidan@simul.co>
2019-09-11 15:15:02 +0100

    Forgot to commit this file change in last commit

--- plugins/UnrealDemo/Plugins/RemotePlay/Source/RemotePlay/Private/Pipelines/EncodePipelineMonoscopic.h
+++ plugins/UnrealDemo/Plugins/RemotePlay/Source/RemotePlay/Private/Pipelines/EncodePipelineMonoscopic.h
@@ -18,19 +18,18 @@ public:
 	void Initialize(const FRemotePlayEncodeParameters& InParams, struct FRemotePlayContext *context, avs::Queue* InColorQueue, avs::Queue* InDepthQueue) override;
 	void Release() override;
 	void PrepareFrame(FSceneInterface* InScene, UTexture* InSourceTexture) override;
-	void EncodeFrame(FSceneInterface* InScene, UTexture* InSourceTexture) override;
+	void EncodeFrame(FSceneInterface* InScene, UTexture* InSourceTexture, FTransform& CameraTransform) override;
 	FSurfaceTexture *GetSurfaceTexture() override
 	{
 		return &ColorSurfaceTexture;
 	}
-	virtual void AddCameraTransform(FTransform& Transform) override;
 	/* End IEncodePipeline interface */
 
 private:
 	void Initialize_RenderThread(FRHICommandListImmediate& RHICmdList);
 	void Release_RenderThread(FRHICommandListImmediate& RHICmdList);
 	void PrepareFrame_RenderThread(FRHICommandListImmediate& RHICmdList, FTextureRenderTargetResource* TargetResource, ERHIFeatureLevel::Type FeatureLevel);
-	void EncodeFrame_RenderThread(FRHICommandListImmediate& RHICmdList);
+	void EncodeFrame_RenderThread(FRHICommandListImmediate& RHICmdList, FTransform CameraTransform);
 
 	template<typename ShaderType>
 	void DispatchProjectCubemapShader(FRHICommandListImmediate& RHICmdList, FTextureRHIRef TextureRHI, FUnorderedAccessViewRHIRef TextureUAVRHI, ERHIFeatureLevel::Type FeatureLevel);
