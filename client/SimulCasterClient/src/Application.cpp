38e007e842016b2664c397348bab8601f7400c80
AndrewRichards-Code <andrew@simul.co>
2019-09-27 11:50:45 +0100

    ~OVRActor() doesn't free geometry or its shader. ~Application() frees the PBR shader.

--- client/SimulCasterClient/src/Application.cpp
+++ client/SimulCasterClient/src/Application.cpp
@@ -91,6 +91,7 @@ Application::~Application()
 	delete mVideoSurfaceTexture;
 	mVideoSurfaceDef.geo.Free();
 	GlProgram::Free(mVideoSurfaceProgram);
+	GlProgram::Free(mEffect.GetGlPlatform());
 
 	delete mSoundEffectPlayer;
 	delete mSoundEffectContext;
