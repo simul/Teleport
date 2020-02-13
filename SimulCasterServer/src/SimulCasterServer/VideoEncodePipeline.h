5ab0c3664dfe7aae486fd3a2f4f9736396a03e03
Aidan Clear <aidan@simul.co>
2020-02-13 11:12:34 +0000

    Adding new classes

---
+++ SimulCasterServer/src/SimulCasterServer/VideoEncodePipeline.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <memory>
+#include "CasterContext.h"
+#include "CasterSettings.h"
+
+// Forward declare so classes that include don't have to know about them
+namespace avs
+{
+	class Pipeline;
+	class Encoder;
+	class Surface;
+}
+
+namespace SCServer
+{
+	struct VideoEncodeParams
+	{
+		int32_t encodeWidth = 0;
+		int32_t encodeHeight = 0;
+		avs::Node* output = nullptr;
+		void* deviceHandle = nullptr;
+		void* inputSurfaceResource = nullptr;
+		GraphicsDeviceType deviceType;
+	};
+
+	class VideoEncodePipeline
+	{
+	public:
+		VideoEncodePipeline() = default;
+		~VideoEncodePipeline() = default;
+
+		Result initialize(const CasterSettings& settings, const VideoEncodeParams& vidEncodeParams);
+		Result process(avs::Transform& cameraTransform, bool forceIDR = false);
+		Result release();
+
+	private:
+		std::unique_ptr<avs::Pipeline> pipeline;
+		std::unique_ptr<avs::Surface> inputSurface;
+		std::unique_ptr<avs::Encoder> encoder;
+	};
+}
