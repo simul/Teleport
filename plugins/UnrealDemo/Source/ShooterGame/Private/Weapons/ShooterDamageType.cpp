1894de8f1fa7717100a60f21317cd9da65c4fa46
Roderick Kennedy <roderick.kennedy@gmail.com>
2020-09-25 20:35:00 +0100

    Remove UnrealDemo, to be re-added as a submodule.

--- plugins/UnrealDemo/Source/ShooterGame/Private/Weapons/ShooterDamageType.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "ShooterGame.h"
-#include "Weapons/ShooterDamageType.h"
-
-UShooterDamageType::UShooterDamageType(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-}
