e37145761f28c9919677e1f555473064edc30c43
GMostyn-Parry <georgemostyn-parry@outlook.com>
2020-10-15 13:40:16 +0100

    Fix Android crash when actor has yet to receive materials.

--- client/SimulCasterClient/src/OVRActorManager.h
+++ client/SimulCasterClient/src/OVRActorManager.h
@@ -14,7 +14,16 @@ public:
 		:Node(id)
 	{}
 
+	virtual ~OVRActor() = default;
+
 	std::vector<OVR::ovrSurfaceDef> ovrSurfaceDefs;
+
+	virtual void SetMesh(std::shared_ptr<scr::Mesh> mesh) override;
+	virtual void SetMaterial(size_t index, std::shared_ptr<scr::Material> material) override;
+	virtual void SetMaterialListSize(size_t size) override;
+	virtual void SetMaterialList(std::vector<std::shared_ptr<scr::Material>>& materials) override;
+private:
+	OVR::ovrSurfaceDef CreateOVRSurface(size_t materialIndex, std::shared_ptr<scr::Material> material);
 };
 
 class OVRActorManager : public scr::ActorManager
@@ -30,10 +39,4 @@ public:
 	//Changes PBR effect used on actors/surfaces to the effect pass with the passed name.
 	//Also changes GlobalGraphicsResource::effectPassName.
     void ChangeEffectPass(const char* effectPassName);
-private:
-    GlobalGraphicsResources& GlobalGraphicsResources = GlobalGraphicsResources::GetInstance();
-
-    //Creates a native actor with the passed id, and actor information.
-    //  actorInfo : Information to use to create the native actor.
-    std::vector<OVR::ovrSurfaceDef> CreateNativeActor(std::shared_ptr<scr::Node> actor);
 };
