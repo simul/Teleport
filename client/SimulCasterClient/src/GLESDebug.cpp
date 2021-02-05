20e0689b56acc9af5a08fe34f8fd67678ee0a9b7
Simul Test <roderick@simul.co>
2021-02-05 13:52:44 +0000

    Ignore low-level GL warnings/info.

--- client/SimulCasterClient/src/GLESDebug.cpp
+++ client/SimulCasterClient/src/GLESDebug.cpp
@@ -5,6 +5,8 @@
 //typedef void (GL_APIENTRY  *GLDEBUGPROC)(GLenum source,GLenum type,GLuint id,GLenum severity,GLsizei length,const GLchar *message,const void *userParam);
 void GL_APIENTRY android_opengles_debug_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* msg, const void* data)
 {
+	if(severity==GL_DEBUG_SEVERITY_LOW||severity==GL_DEBUG_SEVERITY_NOTIFICATION)
+		return;
 	const char *_source;
 	const char *_type;
 	const char *_severity;
