b45a64f49b21ab77c1f8e3aaad2e25ca990e3ff0
GMostyn-Parry <georgemostyn-parry@outlook.com>
2021-05-27 11:05:35 +0100

    Change default parameter of DiscoveryService's back to 0.

--- client/TeleportVRQuestClient/Src/AndroidDiscoveryService.h
+++ client/TeleportVRQuestClient/Src/AndroidDiscoveryService.h
@@ -2,12 +2,10 @@
 
 #include "TeleportClient/DiscoveryService.h"
 
-#include "Config.h"
-
 class AndroidDiscoveryService: public teleport::client::DiscoveryService
 {
 public:
-    AndroidDiscoveryService(uint32_t manualClientID = TELEPORT_DEFAULT_CLIENT_ID);
+    AndroidDiscoveryService(uint32_t manualClientID = 0);
     virtual ~AndroidDiscoveryService();
 
     virtual uint32_t Discover(std::string clientIP, uint16_t clientDiscoveryPort, std::string serverIP, uint16_t serverDiscoveryPort, ENetAddress& remote) override;
--- pc_client/PCDiscoveryService.h
+++ pc_client/PCDiscoveryService.h
@@ -4,12 +4,10 @@
 
 #include <string>
 
-#include "Config.h"
-
 class PCDiscoveryService: public teleport::client::DiscoveryService
 {
 public:
-	PCDiscoveryService(uint32_t manualClientID = TELEPORT_DEFAULT_CLIENT_ID);
+	PCDiscoveryService(uint32_t manualClientID = 0);
 	virtual ~PCDiscoveryService();
 
 	virtual uint32_t Discover(std::string clientIP, uint16_t clientDiscoveryPort, std::string serverIP, uint16_t serverDiscoveryPort, ENetAddress& remote) override;
