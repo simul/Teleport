851310f2d145b3ebd9dcb994b100a7a9a0a5215a
GMostyn-Parry <georgemostyn-parry@outlook.com>
2021-05-27 09:27:37 +0100

    Fix android compilation, and give Android a reasonable default client ID.
    # Config.h on PC Client and Android now contain the exact same definitions.

--- pc_client/PCDiscoveryService.h
+++ pc_client/PCDiscoveryService.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "TeleportClient/DiscoveryService.h"
+
 #include <string>
+
+#include "Config.h"
 
 class PCDiscoveryService: public teleport::client::DiscoveryService
 {
 public:
-	PCDiscoveryService(uint32_t manualClientID = 0);
+	PCDiscoveryService(uint32_t manualClientID = TELEPORT_DEFAULT_CLIENT_ID);
 	virtual ~PCDiscoveryService();
 
 	virtual uint32_t Discover(std::string clientIP, uint16_t clientDiscoveryPort, std::string serverIP, uint16_t serverDiscoveryPort, ENetAddress& remote) override;
