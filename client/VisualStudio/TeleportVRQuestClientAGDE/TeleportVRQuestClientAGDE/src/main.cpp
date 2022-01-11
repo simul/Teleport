f537703b5cb1c4f56b885271edd70929133c39ba
Roderick Kennedy <roderick.kennedy@gmail.com>
2022-01-11 14:06:07 +0000

    Relative paths and missing files for Android Visual Studio code.

--- client/VisualStudio/TeleportVRQuestClientAGDE/TeleportVRQuestClientAGDE/src/main.cpp
+++ client/VisualStudio/TeleportVRQuestClientAGDE/TeleportVRQuestClientAGDE/src/main.cpp
@@ -34,6 +34,7 @@ Platform gPlatform = Platform::ANDROID;
 
 #include "Platform/Vulkan/RenderPlatform.h"
 #include "Platform/Vulkan/DeviceManager.h"
+#include <unistd.h>
 
 using namespace simul;
 using namespace crossplatform;
@@ -279,8 +280,14 @@ void handle_cmd(android_app* app, int32_t cmd)
 		__android_log_write(ANDROID_LOG_INFO, "TeleportVRQuestClientAGDE", "Event not handled");
 	}
 }
+//static bool x = true;
 void android_main(struct android_app* app)
 {
+	/*while (x)
+	{
+		__android_log_write(ANDROID_LOG_INFO, "TeleportVRQuestClientAGDE", "Waiting");
+		sleep(100);
+	}*/
 	app->onAppCmd = handle_cmd;
 	int events;
 	android_poll_source* source;
