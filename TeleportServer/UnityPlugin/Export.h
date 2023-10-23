f431e54fa8a22fb894d465d7de1c553068fd7424
Simul Test <roderick@simul.co>
2023-10-23 01:01:13 +0100

    .

--- TeleportServer/UnityPlugin/Export.h
+++ TeleportServer/UnityPlugin/Export.h
@@ -1,6 +1,10 @@
 #pragma once
+#ifdef DOXYGEN
+#define TELEPORT_EXPORT
+#else
 #if PLATFORM_WINDOWS
 #define TELEPORT_EXPORT extern "C" __declspec(dllexport)
 #else
 #define TELEPORT_EXPORT extern "C" __attribute__((visibility("default")))
+#endif
 #endif
